b0712119351cf6a882c754eefc7a7f02ef4943e0
praks-mobiledi <prakash@mobile-di.com>
2015-06-13 09:27:55 +0530

    Added authentication test case

--- d_jour_nosql/src/main/java/com/mobiledi/d_jour_nosql/Constants.java
+++ d_jour_nosql/src/main/java/com/mobiledi/d_jour_nosql/Constants.java
@@ -2,71 +2,71 @@ package com.mobiledi.d_jour_nosql;
 
 public final class Constants {
 	
-	final static String USERNAME = "username";
-	final static String PASSWORD = "password";
+	final public static String USERNAME = "username";
+	final public static String PASSWORD = "password";
 	
 	
 	
 
 	/* MASTER TABLE details */
-	final static String TABLE_RESTAURANT_MASTER = "restaurant_master";
-	final static String COLUMN_MASTER_ID="id";
-	final static String COLUMN_NAME = "name";
-	final static String COLUMN_TITLE = "title";
-	final static String COLUMN_EMAIL = "email";
-	final static String COLUMN_BANNER_IMAGE = "banner_image";
-	final static String COLUMN_PHONE = "phone";
-	final static String COLUMN_WEBSITE = "website";
-	final static String COLUMN_PASSWORD = "password";
+	final public static String TABLE_RESTAURANT_MASTER = "restaurant_master";
+	final public static String COLUMN_MASTER_ID="id";
+	final public static String COLUMN_NAME = "name";
+	final public static String COLUMN_TITLE = "title";
+	final public static String COLUMN_EMAIL = "email";
+	final public static String COLUMN_BANNER_IMAGE = "banner_image";
+	final public static String COLUMN_PHONE = "phone";
+	final public static String COLUMN_WEBSITE = "website";
+	final public static String COLUMN_PASSWORD = "password";
 	
 	/* ADDRESS TABLE details */
-	final static String TABLE_RESTAURANT_ADDRESS = "restaurant_address";	
-	final static String COLUMN_MASTER_ID_ADDRESS = "fk_restaurant_master_id";
-	final static String COLUMN_ADD1 = "address_line1";
-	final static String COLUMN_ADD2 = "address_line2";
-	final static String COLUMN_CITY = "city";
-	final static String COLUMN_STATE = "state";
-	final static String COLUMN_ZIP = "zip";
-	final static String COLUMN_LAT = "lat";
-	final static String COLUMN_LONG = "long";
-	final static String COLUMN_ACTIVE="active_flag";
-	final static String COLUMN_CREATED="create_date";
+	final public static String TABLE_RESTAURANT_ADDRESS = "restaurant_address";	
+	final public static String COLUMN_MASTER_ID_ADDRESS = "fk_restaurant_master_id";
+	final public static String COLUMN_ADD1 = "address_line1";
+	final public static String COLUMN_ADD2 = "address_line2";
+	final public static String COLUMN_CITY = "city";
+	final public static String COLUMN_STATE = "state";
+	final public static String COLUMN_ZIP = "zip";
+	final public static String COLUMN_LAT = "lat";
+	final public static String COLUMN_LONG = "long";
+	final public static String COLUMN_ACTIVE="active_flag";
+	final public static String COLUMN_CREATED="create_date";
 	
 	/* HOURS TABLE details */
-	final static String TABLE_RESTAURANT_HOURS = "restaurant_hours";
-	final static String COLUMN_MASTER_ID_HOURS = "fk_restaurant_master_id";
-	final static String COLUMN_WEEKDAY_OPENING_HOUR = "weekday_opening_hour";
-	final static String COLUMN_WEEKDAY_OPENING_MINUTES = "weekday_opening_minutes";
-	final static String COLUMN_WEEKDAY_CLOSING_HOUR="weekday_closing_hour";
-	final static String COLUMN_WEEKDAY_CLOSING_MINUTES ="weekday_closing_minutes";
-	final static String COLUMN_WEEKEND_OPENING_HOUR = "weekend_opening_hour";
-	final static String COLUMN_WEEKEND_OPENING_MINUTES = "weekend_opening_minutes";
-	final static String COLUMN_WEEKEND_CLOSING_HOUR="weekend_closing_hour";
-	final static String COLUMN_WEEKEND_CLOSING_MINUTES ="weekend_closing_minutes";
-	final static String COLUMN_ACTIVE_HOURS="active_flag";
-	final static String COLUMN_CREATED_HOURS="create_datetime";
+	final public static String TABLE_RESTAURANT_HOURS = "restaurant_hours";
+	final public static String COLUMN_MASTER_ID_HOURS = "fk_restaurant_master_id";
+	final public static String COLUMN_WEEKDAY_OPENING_HOUR = "weekday_opening_hour";
+	final public static String COLUMN_WEEKDAY_OPENING_MINUTES = "weekday_opening_minutes";
+	final public static String COLUMN_WEEKDAY_CLOSING_HOUR="weekday_closing_hour";
+	final public static String COLUMN_WEEKDAY_CLOSING_MINUTES ="weekday_closing_minutes";
+	final public static String COLUMN_WEEKEND_OPENING_HOUR = "weekend_opening_hour";
+	final public static String COLUMN_WEEKEND_OPENING_MINUTES = "weekend_opening_minutes";
+	final public static String COLUMN_WEEKEND_CLOSING_HOUR="weekend_closing_hour";
+	final public static String COLUMN_WEEKEND_CLOSING_MINUTES ="weekend_closing_minutes";
+	final public static String COLUMN_ACTIVE_HOURS="active_flag";
+	final public static String COLUMN_CREATED_HOURS="create_datetime";
 	
 	
 	/*PROFILE TAGS*/
-	final static String TABLE_PROFILE_TAGS = "profile_tags";
-	final static String COLUMN_ID_TAGS = "id";
-	final static String COLUMN_TAG_KEY = "tag_key";
-	final static String COLUMN_TAG_VALUE = "tag_value";
+	final public static String TABLE_PROFILE_TAGS = "profile_tags";
+	final public static String COLUMN_ID_TAGS = "id";
+	final public static String COLUMN_TAG_KEY = "tag_key";
+	final public static String COLUMN_TAG_VALUE = "tag_value";
 	
 	
 	/* TAGS TABLE details */
-	final static String TABLE_RESTAURANT_TAGS = "restaurant_tags";
-	final static String COLUMN_MASTER_ID_TAGS = "fk_restaurant_id";
-	final static String COLUMN_PROFILE_TAGS = "fk_profile_tags_id";
+	final public static String TABLE_RESTAURANT_TAGS = "restaurant_tags";
+	final public static String COLUMN_MASTER_ID_TAGS = "fk_restaurant_id";
+	final public static String COLUMN_PROFILE_TAGS = "fk_profile_tags_id";
 	
 	/* RESTAURANT FEED ITEMS*/
-	final static String TABLE_RESTAURANT_FEED_ITEMS = "restaurant_feed_items";
-	final static String COLUMN_ID_FEED = "id";
-	final static String COLUMN_MASTER_ID_FEED = "fk_restaurant_master";
-	final static String COLUMN_FEED_TITLE = "feed_title";
-	final static String COLUMN_FEED_DESCRIPTION = "feed_description";
-	final static String COLUMN_FEED_IMAGE = "feed_image";
-	final static String COLUMN_FEED_SEQUENCE_NUMBER = "fee_sequence_number";
-	final static String COLUMN_FEED_CREATE_DATE = "create_date";
-	final static String COLUMN_FEED_UPDATE_TIME = "update_date";
+	final public static String TABLE_RESTAURANT_FEED_ITEMS = "restaurant_feed_items";
+	final public static String COLUMN_ID_FEED = "id";
+	final public static String COLUMN_MASTER_ID_FEED = "fk_restaurant_master";
+	final public static String COLUMN_FEED_TITLE = "feed_title";
+	final public static String COLUMN_FEED_DESCRIPTION = "feed_description";
+	final public static String COLUMN_FEED_IMAGE = "feed_image";
+	final public static String COLUMN_FEED_SEQUENCE_NUMBER = "fee_sequence_number";
+	final public static String COLUMN_FEED_CREATE_DATE = "create_date";
+	final public static String COLUMN_FEED_UPDATE_TIME = "update_date";
 }
--- d_jour_nosql/src/test/java/com/mobiledi/d_jour_nosql/test/JunitTest.java
+++ d_jour_nosql/src/test/java/com/mobiledi/d_jour_nosql/test/JunitTest.java
@@ -2,6 +2,15 @@ package com.mobiledi.d_jour_nosql.test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.JsonNodeFactory;
+import org.codehaus.jackson.node.ObjectNode;
+
+import com.mobiledi.d_jour_nosql.Constants;
 
 import junit.framework.TestCase;
 
@@ -14,7 +23,8 @@ public class JunitTest extends TestCase {
 	final static String URL = "jdbc:postgresql://" + DB_IP + "/" + DATABASE;
 	Connection connection = null;
 	
-	
+	final static String USER_USERNAME = "cp@gmail.com";
+	final static String USER_PASSWORD = "login123";
 	
 	protected static void setUpBeforeClass() throws Exception {
 	}
@@ -37,13 +47,51 @@ public class JunitTest extends TestCase {
 		assertTrue(true);
 	}
 
-	public void testDBConnection() {
-		
+	public void testDBConnection() {	
 		boolean connected=connectToDB();
 		assertEquals(true, connected);
-
 	}
 	
+	/*Check if a user is registered in the db*/
+	public void testUserRegisteredinportal() {
+		ObjectNode obj= new ObjectNode(JsonNodeFactory.instance);
+		obj.put("username", USER_USERNAME);
+		obj.put("password", USER_PASSWORD);
+		 JsonNode toAuthenticate=obj;
+		
+		
+	if (connectToDB()) {	
+		String username = toAuthenticate.get(Constants.USERNAME).asText();
+		String password = toAuthenticate.get(Constants.PASSWORD).asText();
+		
+		StringBuilder masterID = new StringBuilder();
+		masterID.append("SELECT * FROM ");
+		masterID.append(Constants.TABLE_RESTAURANT_MASTER);
+		masterID.append(" WHERE " + Constants.COLUMN_EMAIL + "= '"+ username +"' AND " +Constants.COLUMN_PASSWORD + "='"+ password+"'");
+		System.out.println("GENERATED SELECT QUERY: " + masterID.toString());
+		try {
+			PreparedStatement masterid = connection
+					.prepareStatement(masterID.toString());
+			
+			ResultSet results=masterid.executeQuery();
+			assertEquals("User is registered:", true, results.next());
+			results.close();
+			
+		}
+		catch(SQLException e){
+			
+			e.printStackTrace();
+		}
+		
+		disconnectFromDB();
+		
+	}
+	else{
+		
+		assertNotNull("Can't connect to db", connection);
+	}
+	
+	}
 	
 	private boolean connectToDB() {
 		try {
@@ -56,8 +104,7 @@ public class JunitTest extends TestCase {
 			}
 
 			else {
-				System.out.println("Connected to POSTGRES db "
-						+ connection.toString());
+				System.out.println("Connected to POSTGRES db ");
 				return true;
 			}
 
@@ -67,5 +114,15 @@ public class JunitTest extends TestCase {
 
 	}
 	
+	private void disconnectFromDB() {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
 
 }
